f8d9accb18309c3e90296857c5c6341212af1f3f
o0o52lin <237277669@qq.com>
2023-09-08 10:43:19 +0800

    update

--- zb_users/plugin/staticize/exec.php
+++ zb_users/plugin/staticize/exec.php
@@ -25,21 +25,9 @@ exec($cmd, $output, $returnValue);
 
 // 检查返回值以确定命令是否成功执行
 if ($returnValue === 0) {
-    // 命令执行成功
-    echo "Command executed successfully.\n";
-    
     // 输出正确的结果
-    echo "Output:\n";
-    foreach ($output as $line) {
-        echo $line . "\n";
-    }
+    echo nl2b("Command executed successfully.\n\nOutput:\n".$output);
 } else {
-    // 命令执行失败
-    echo "Command failed to execute.\n";
-    
     // 输出错误信息
-    echo "Error:\n";
-    foreach ($output as $line) {
-        echo $line . "\n";
-    }
+    echo nl2b("Command failed to execute.\n\nError:\n".$output);
 }
