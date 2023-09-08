3b9372bbc30decbffb7e092c704548b9a23167e0
o0o52lin <237277669@qq.com>
2023-09-08 10:04:02 +0800

    del

--- zb_users/plugin/o0oLoginReg/include.php
+++
@@ -1,7 +0,0 @@
-<?php
-#注册插件
-RegisterPlugin("o0oLoginReg","ActivePlugin_o0oLoginReg");
-
-function ActivePlugin_o0oLoginReg() {}
-function InstallPlugin_o0oLoginReg() {}
-function UninstallPlugin_o0oLoginReg() {}
--- zb_users/plugin/staticize/include.php
+++
@@ -1,8 +0,0 @@
-<?php
-RegisterPlugin("staticize", "ActivePlugin_staticize");
-
-function ActivePlugin_staticize(){}
-
-function InstallPlugin_staticize(){}
-
-function UninstallPlugin_staticize(){}
