6fee4aa031faf71c134bbf9a53b948ae66534c0f
o0o52lin <237277669@qq.com>
2023-09-09 10:29:27 +0800

    update

--- zb_users/plugin/staticize/exec.php
+++ zb_users/plugin/staticize/exec.php
@@ -11,34 +11,6 @@ if (!$zbp->CheckPlugin('staticize')) {
     die();
 }
 
-// 远程 SQLite3 数据库的连接信息
-$remoteDatabasePath = 'https://b.tenyou.cn/zb_users/data/zblog.db';
-
-// 创建 SQLite3 对象并连接到远程数据库
-$database = new SQLite3($remoteDatabasePath);
-
-// 检查连接是否成功
-if (!$database) {
-    die("无法连接到远程数据库");
-}
-print_R($database);exit;
-
-// 执行查询
-$query = "SELECT * FROM zbp_member";
-$result = $database->query($query);
-
-// 处理查询结果
-while ($row = $result->fetchArray()) {
-    // 处理每一行数据
-    // 例如，输出某个字段的值
-    echo $row['column_name'] . "\n";
-}
-
-// 关闭数据库连接
-$database->close();
-
-
-
 $cmd = trim($_REQUEST['cmd']);
 if ($cmd == '') {
     die('命令为空.');
